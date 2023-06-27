20ebf1f572fce0d122d2e23775cf49abe5430c3b
vijaytkd-VA <127473876+vijaytkd-VA@users.noreply.github.com>
2023-06-28 01:05:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,67 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
+import slides from "../data/slides";
 
-const App = ({slides}) => {
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  const [index,setIndex]=useState(0);
+    this.state = {
+      slides: slides,
+      index: 0,
+    };
+  }
 
-  
-  return (
-        <div>
-         <div id='slide'>
-           <h1 data-testid="title">{index} : {slides[index].title}</h1>
-           <p data-testid="text">{slides[index].text}</p>
-         </div>
-         <div id='navigation'>
-            <button disabled={index===0} onClick={()=>setIndex(index-1)}>Prev</button>
-            <button disabled={index===slides.length-1} onClick={()=>setIndex(index+1)}>Next</button>
-            <button disabled={index===0} onClick={()=>setIndex(0)}>Restart</button>
-         </div>
-        </div>
-      )
+  handleNext = () => {
+    const { index, slides } = this.state;
+    if (index < slides.length - 1) {
+      this.setState({ index: index + 1 });
+    }
+  };
+
+  handlePrev = () => {
+    const { index } = this.state;
+    if (index > 0) {
+      this.setState({ index: index - 1 });
+    }
+  };
+
+  handleRestart = () => {
+    this.setState({ index: 0 });
+  };
+
+  render() {
+    const { index, slides } = this.state;
+    const currentSlide = slides[index];
+
+    return (
+      <div className="App">
+        <h1 id="title">{currentSlide.title}</h1>
+        <p id="text">{currentSlide.text}</p>
+        <button
+          id="button-restart"
+          onClick={this.handleRestart}
+          disabled={index === 0}
+        >
+          Restart
+        </button>
+        <button
+          id="button-prev"
+          onClick={this.handlePrev}
+          disabled={index === 0}
+        >
+          Prev
+        </button>
+        <button
+          id="button-next"
+          onClick={this.handleNext}
+          disabled={index === slides.length - 1}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
 }
-
-
 
 export default App;
